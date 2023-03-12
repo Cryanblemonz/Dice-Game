8cb4a37bc21c8d2d4aead62ce08ffa2fcd4149b0
George Bryan Clemons <gbc466@gmail.com>
2023-03-12 16:13:31 -0500

    ~Added button allowing user to roll without refreshing ~Added scoreboard

--- Index.js
+++ Index.js
@@ -1,16 +1,36 @@
-let randomNumber1 = Math.floor(Math.random()*6+1);
-let randomNumber2 = Math.floor(Math.random()*6+1);
 
-console.log(randomNumber1);
+let score1 = 0;
+let score2 = 0;
+
+let rollable = true;
+
+document.querySelector(".roll").addEventListener('click', function() {
+    buttonAnimation();
+    let randomNumber1 = Math.floor(Math.random()*6+1);
+    let randomNumber2 = Math.floor(Math.random()*6+1);
+    let scoreBoard1 = document.querySelector('.score1');
+    let scoreBoard2 = document.querySelector('.score2');
 
 
-document.querySelector(".img1").setAttribute('src', 'images/dice' + randomNumber1 + '.png');
-document.querySelector(".img2").setAttribute('src', 'images/dice' + randomNumber2 + '.png');
-
-if(randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = " 🚩 Player 1 Wins!";
-} else if(randomNumber2 > randomNumber1) {
-    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
-} else if(randomNumber2 == randomNumber1) {
-    document.querySelector("h1").innerHTML = "Draw!";
+    document.querySelector(".img1").setAttribute('src', 'images/dice' + randomNumber1 + '.png');
+    document.querySelector(".img2").setAttribute('src', 'images/dice' + randomNumber2 + '.png');
+    
+    if(randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = " 🚩 Player 1 Wins!";
+        score1++;
+    } else if(randomNumber2 > randomNumber1) {
+        document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+        score2++;
+    } else if(randomNumber2 == randomNumber1) {
+        document.querySelector("h1").innerHTML = "Draw!";
+    }
+    console.log(score1, score2);
+    scoreBoard1.innerHTML = "Player 1: " + score1;
+    scoreBoard2.innerHTML = "Player 2: " + score2;
+});
+ 
+function buttonAnimation(){
+    document.getElementById('button').classList.add('pressed');
+    setTimeout(function(){
+        document.getElementById('button').classList.remove('pressed')}, 100);
 }
